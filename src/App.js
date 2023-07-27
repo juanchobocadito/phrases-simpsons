0b1310e866a463456f6315c0ed13daed2a31e260
juanchobocadito <juan.cabrerar@pca.edu.co>
2023-07-27 16:29:50 -0500

    proyectof

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Container from "./components/Container";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import CharacterContainer from "./components/CharacterContainer";
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async () => {
+    const api = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes?count=3"
+    );
+    const frase = await api.json();
+    setCharacters(frase);
+  };
+
+  return (
+    <Container>
+      <Header />
+      {!characters ? (
+        <Welcome reqApi={reqApi} />
+      ) : (
+        <CharacterContainer characters={characters} reqApi={reqApi} />
+      )}
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function Header() {
+    return <Title>Las mejores Frases de los Simpsons</Title>;
+}
+
+const Title = styled.h1`
+      color: #e76f51;
+      text-align: center;
+
+`;
